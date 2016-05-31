30caa39389f446efd3f1717763ad54defb07838e
DiegoArmandoRosasLuna <darl1692@hotmail.com>
2016-05-31 12:47:46 -0500

    Add files via upload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -31,17 +31,27 @@ jQuery(document).ready(function() {
 });
 function validar(){
     var email = $('#email').val() , pass =$('#password').val();
-    $.post('assets/db/login.php',{
+    $.post('include/db/login.php',{
         email:email, pass:pass
     },function (data){
-       // alert(data);
-            if (data == 1) {         
-                    window.location='views/prueba1.php';
-                    
+        //alert(data);
+            if (data==0) {
+                window.location='setpass.php?email='+email;
 
             }else{
-                    $('.form-top-left').html("<h3>Iniciar sesión</h3><p>Email o Contraseña incorrectos<img src='assets/images/IconError.png' alt='Error' height=19px></img></p>");
-                    $('.login-form input[type="text"], .login-form input[type="password"]').addClass('input-error');
+                if (data == 1) {
+                    window.location='views/home.php';
+
+                }else{
+                    if (data== 2) {
+                        $('#errorLog').html("<div class='alert alert-danger' role='alert'><p><i class='glyphicon glyphicon-remove'></i> Email o contraseña incorrectos</p></div>");
+                        $('.login-form input[type="text"], .login-form input[type="password"]').addClass('input-error');
+
+                    }else{
+                        alert(data);
+                    }
+                }
+
             }
     });
 
@@ -49,15 +59,54 @@ function validar(){
 
 function sendpass(){
     var email = $('#email').val();
-    $.post('assets/db/recovery.php',{
+    $.post('include/db/recovery.php',{
         email:email
     },function (data1){
        // alert(data1);
         if (data1==0) {
-            $('.form-top-left').html("<h3>Recuperar contraseña</h3><p>El email no se encuentra registrado<img src='assets/images/IconError.png' alt='Error' height=19px></img></p>");
+            $('#errorLog').html("<div class='alert alert-danger' role='alert'><p><i class='glyphicon glyphicon-remove'></i> El email indicado no existe, verifícalo nuevamente</p></div>");
             $('.login-form input[type="text"]').addClass('input-error');
         }else{
-            $('.form-top-left').html("<h3>Recuperar contraseña</h3><p>Email enviado</p>");
+            $('#errorLog').html("<div class='alert alert-success' role='alert'><p><i class='glyphicon glyphicon-ok'></i> Email enviado correctamente</p></div>");
+            $('#btn-enviar').html("<a href='index.php' class='btn white-txt'>INICIAR SESIÓN</a>");
+            $('')
+        }
+    });
+}
+
+function nuevacontraseña(){
+        var password = $('#pass1').val(), pass = $('#pass2').val();
+        var email = $('#hidden').val();
+
+        if (password==pass) {
+            $.post('include/db/newpassword.php',{password:password, email:email},function(data2){
+                alert(data2);
+
+                if (data2 == 1) {
+                    $('#errorLog').html("<div><p>Contraseña creada con éxito. <a href='index.php'>INICIA SESIÓN</a></p></div>");
+                }else{                                
+                    alert(data);
+                }
+
+            });        
+        }else{
+            
+            $('#errorLog').html("<div class='alert alert-danger' role='alert'><p><i class='glyphicon glyphicon-remove'></i> Verifica que las contraseñas sean iguales</p></div>");
+            
+        }
+}
+function newuser(){
+    var user = $('#email').val();
+    $.post('include/db/newuserdb.php',{user:user},function(data3){
+        
+        if (data3==1) {
+            $('#errorLog').html("<div class='alert alert-danger' role='alert'><p><i class='glyphicon glyphicon-remove'></i> El usuario no existe que mas va a realizar?</p></div>");         
+        }else{
+            if (data3==2) {
+                $('#errorLog').html("<div class='alert alert-danger' role='alert'><p><i class='glyphicon glyphicon-remove'></i> El usuario ya existe</p></div>");         
+            }else{
+                alert(data3);    
+            }
         }
     });
 }
